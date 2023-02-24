8e3cd87087e47f9f9c03f23ea055451fcd40157d
kinddoctor <tata-yagodina@yandex.ru>
2023-02-25 00:27:13 +0500

    fix function

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,6 +1,10 @@
 const capitalize = (text) => {
-  const [firstChar, ...restChars] = text;
-  return `${firstChar.toUpperCase()}${restChars.join('')}`;
+  if (text === '') {
+    return '';
+  }
+  const firstChar = text[0].toUpperCase();
+  const restSubstring = text.slice(1);
+  return `${firstChar}${restSubstring}`;
 };
 
 export default capitalize;
